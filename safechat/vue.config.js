2f72b5e229f8339f73de9465d2e84cc2c5234e2c
Pranjalya <pranjalyawarrior@gmail.com>
2020-07-17 15:53:43 +0530

    ui deployed

---
+++ safechat/vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  pluginOptions: {
+    quasar: {
+      importStrategy: 'kebab',
+      rtlSupport: false
+    },
+    cordovaPath: 'src-cordova'
+  },
+
+  transpileDependencies: [
+    'quasar'
+  ],
+
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/safechat/'
+  : '/'
+}
